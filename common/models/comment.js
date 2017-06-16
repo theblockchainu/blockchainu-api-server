8041bc63b399ea670b2d1fa078627a33e81fef9e
mtausifsheikh <mtausif.sheikh@gmail.com>
2017-06-16 11:18:19 +0530

    unwanted code removed

--- common/models/comment.js
+++ common/models/comment.js
@@ -2,17 +2,4 @@
 
 module.exports = function(Comment) {
     
-    Comment.afterRemote('prototype.__create__replies', function (ctx, newInstance, next) {
-        
-        if (ctx.req.user) {
-            var userId = ctx.req.user.id;
-            Comment.app.models.peer.findById(userId, function (err, instance) {
-                newInstance.peer.add(instance, function (err, addedinstance) {
-                    console.log("Relation created between reply and peer.");
-                });
-            });
-        } else
-            console.log("User Not Signed in!");
-        next();
-    });
 };
