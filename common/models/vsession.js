df94533c1db8993a63040940ecc1cb7bbd8b4075
Abhijeet Chakraborty <abhijeet1abhijeet@gmail.com>
2017-08-29 18:32:48 +0530

    calendarevents fixed, vsession fixed

--- common/models/vsession.js
+++ common/models/vsession.js
@@ -1,4 +1,5 @@
 'use strict';
+var app = require('../../server/server');
 var accountSid = app.get('twilioSID');
 var apiKeySid = app.get('twilioKey');
 var apiKeySecret = app.get('twilioSecret');
@@ -6,7 +7,6 @@ const Twilio = require('twilio');
 const Video = require('twilio-video');
 const AccessToken = Twilio.jwt.AccessToken;
 var VideoGrant = AccessToken.VideoGrant;
-
 module.exports = function (Vsession) {
 
     Vsession.getToken = function (req, cb) {
@@ -17,7 +17,10 @@ module.exports = function (Vsession) {
             token.identity = loggedinPeer.id;
             var grant = new VideoGrant();
             token.addGrant(grant);
-            cb(token);
+            cb(null, {
+                identity: token.identity,
+                token: token.toJwt()
+            });
         } else {
             var err = new Error('Invalid access');
             err.code = 'INVALID_ACCESS';
