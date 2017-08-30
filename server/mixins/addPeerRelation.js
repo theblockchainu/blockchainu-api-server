60c8061e5a060d18d0036b22797f62c4a6f1d206
donatedTunic1290 <connect@aakashbansal.com>
2017-08-30 17:10:06 +0530

    Fixed peer relation mixin

--- server/mixins/addPeerRelation.js
+++ server/mixins/addPeerRelation.js
@@ -7,16 +7,20 @@ module.exports = function (Model, options) {
 
         Model.afterRemote(methodName, function (ctx, newInstance, next) {
             //newInstance is the instance of the created relations Model
-            if (ctx.req.cookies.userId) {
-                var userId = ctx.req.cookies.userId.split(/[ \:.]+/)[1];
+            if (ctx.req.headers.cookie) {
+                var cookie = ctx.req.headers.cookie.split(';')[2].split('=')[1].trim();
+                var userId = cookie.split(/[ \:.]+/)[0].substring(4);
+                console.log('user ID from cookie is: ' + userId);
                 Model.app.models.peer.findById(userId, function (err, instance) {
                     if (err) {
                         console.log("User Not Found");
                     } else {
-                        newInstance.peer.add(instance, function (err, addedinstance) {
+                        newInstance.peer.add(instance.id, function (err, addedinstance) {
                             if (err) {
                                 console.log("Unable to add peer to " + element);
+                                console.log(err);
                             } else {
+                                console.log(addedinstance);
                                 console.log("Peer Instance Added to " + element);
                             }
                         });
