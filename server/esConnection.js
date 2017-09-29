aafd8d14fadc39129dafbf5514446eb5583d363e
donatedTunic1290 <connect@aakashbansal.com>
2017-09-29 16:47:40 +0530

    - Corrected signup logic to avoid existing user signup.

--- server/esConnection.js
+++ server/esConnection.js
@@ -17,28 +17,6 @@ var client = new elasticsearch.Client( {
     apiVersion: "5.x",
     log: "trace",
     requestTimeout: 30000
-    /*settings : {
-        "number_of_shards": 1,
-        "analysis" : {
-            "filter" : {
-                "autocomplete_filter": {
-                    "type" : "edge_ngram",
-                    "min_gram" : 1,
-                    "max_gram" : 20
-                }
-            },
-            "analyzer" : {
-                "autocomplete": {
-                    "type" : "custom",
-                    "tokenizer": "standard",
-                    "filter" : [
-                        "lowercase",
-                        "autocomplete_filter"
-                    ]
-                }
-            }
-        }
-    }*/
 });
 
 module.exports = client;
