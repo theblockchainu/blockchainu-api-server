ba81a863e4c23cf4713e5ab117d1ddaa4ddfe5ad
abhijeet1403 <abhijeet1abhijeet@gmail.com>
2018-04-26 11:51:24 +0530

    validating enum value

--- common/models/assessment-na-rule.js
+++ common/models/assessment-na-rule.js
@@ -1,5 +1,5 @@
 'use strict';
 
-module.exports = function(Assessmentnarule) {
-
+module.exports = function (Assessmentnarule) {
+    Assessmentnarule.validatesInclusionOf('value', { in: ['attendance', 'community'] });
 };
