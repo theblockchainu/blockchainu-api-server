5e2070568b36920b09cf2fe85cc4e5c4a9751a41
abhijeet <abhijeet1abhijeet@gmail.com>
2018-06-20 19:14:15 +0530

    fix

--- common/models/accreditation.js
+++ common/models/accreditation.js
@@ -1,5 +1,34 @@
 'use strict';
 
-module.exports = function(Accreditation) {
+module.exports = function (Accreditation) {
+    Accreditation.afterRemote('prototype.__link__subscribedBy', function (ctx, peerInstance, next) {
+        let accreditationInstance = ctx.instance;
+        console.log('inside the join hook of accreditation');
+
+        // const message = {};
+        // var renderer = loopback.template(path.resolve(__dirname, '../../server/views/newResultStudent.ejs'));
+        // var html_body = renderer(message);
+        // loopback.Email.send({
+        //     to: assessmentResultInstance.toJSON().assessees[0].email,
+        //     from: 'Peerbuds <noreply@mx.peerbuds.com>',
+        //     subject: 'Your result for ' + collectionType + ': ' + collectionTitle,
+        //     html: html_body
+        // })
+        //     .then(function (response) {
+        //         console.log('email sent! - ');
+        //     })
+        //     .catch(function (err) {
+        //         console.log('email error! - ' + err);
+        //     });
+        console.log(accreditationInstance);
+        next();
+    });
+
+    // Record accreditation participation on BC.
+    Accreditation.afterRemote('prototype.__unlink__subscribedBy', function (ctx, peerInstance, next) {
+        let accreditationInstance = ctx.instance;
+        console.log(accreditationInstance);
+        next();
+    });
 
 };
