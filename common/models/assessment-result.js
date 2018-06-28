457537234632b006a9532055811a2d3754fc19cc
donatedTunic1290 <connect@aakashbansal.com>
2018-06-28 09:50:10 +0530

    Fixes

--- common/models/assessment-result.js
+++ common/models/assessment-result.js
@@ -37,23 +37,6 @@ module.exports = function (Assessmentresult) {
 			.then((assessmentResultInstance) => {
 				// Record assessment on BC
 				const assessmentResultInstanceJSON = assessmentResultInstance.toJSON();
-				request
-					.put({
-						url: protocolUrl + 'collections/' + assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].id + '/peers/' + assessmentResultInstanceJSON.assessees[0].ethAddress,
-						body: {
-							assessmentResult: assessmentResultInstanceJSON.assessment_rules[0].value,
-							engagementResult: assessmentResultInstanceJSON.assessmentEngagementResult,
-							commitmentResult: assessmentResultInstanceJSON.assessmentCommitmentResult,
-							hash: '' // hash
-						},
-						json: true
-					}, function (err, response, data) {
-						if (err) {
-							console.error(err);
-						} else {
-							console.log('Recorded assessment on blockchain: ' + data);
-						}
-					});
 
 				if (assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].certificate_templates && assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].certificate_templates.length > 0) {
 					const certificateTemplate = assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].certificate_templates[0];
@@ -173,7 +156,8 @@ module.exports = function (Assessmentresult) {
 										if (err) {
 											console.error(err);
 										} else {
-											console.log('Recorded assessment on blockchain: ' + data);
+											console.log('Recorded assessment on blockchain: ');
+											console.log(data);
 											const signature = {
 												"type": [
 													"sha256"
