cb27fbff7d27b1bcb3b25f799af8bdd923c79e52
abhijeet <abhijeet1abhijeet@gmail.com>
2018-08-22 18:02:34 +0530

    contact us form

--- common/models/guest-contact.js
+++ common/models/guest-contact.js
@@ -2,7 +2,7 @@
 let loopback = require('loopback');
 let path = require('path');
 
-module.exports = function(Guestcontact) {
+module.exports = function (Guestcontact) {
 
     Guestcontact.observe('after save', function sendEmail(ctx, next) {
         if (!ctx.instance && !ctx.data) return next();
@@ -14,7 +14,7 @@ module.exports = function(Guestcontact) {
 
         let message, subject;
         message = {
-            heading: 'Hi '+ name +', \nWe have received your query and will get back to you with the best possible response at the earliest.'
+            heading: 'Hi ' + name + ', \nWe have received your query and will get back to you with the best possible response at the earliest.'
         };
         subject = 'Thanks for reaching out';
 
@@ -33,6 +33,26 @@ module.exports = function(Guestcontact) {
                 console.log('email error! - ' + err);
             });
 
+        let messageAdmin = {
+            userName: ctx.instance.first_name + ' ' + ctx.instance.last_name,
+            userEmail: ctx.instance.email,
+            message: ctx.instance.message
+        };
+        let rendererAdmin = loopback.template(path.resolve(__dirname, '../../server/views/newQueryAdmin.ejs'));
+        let html_admin_body = rendererAdmin(messageAdmin);
+        loopback.Email.send({
+            to: 'aakash@peerbuds.com',
+            from: 'Peerbuds <noreply@mx.peerbuds.com>',
+            subject: 'New User Query! for' + ctx.instance.message,
+            html: html_admin_body
+        })
+            .then(function (response) {
+                console.log('email sent! - ' + response);
+            })
+            .catch(function (err) {
+                console.log('email error! - ' + err);
+            });
+        next(null, 'Success');
     });
 
 };
