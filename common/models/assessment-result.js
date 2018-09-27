c4e1f374d6085f34023074a84fb2dd5b773a21a8
donatedTunic1290 <connect@aakashbansal.com>
2018-09-28 05:09:34 +0530

    fixes

--- common/models/assessment-result.js
+++ common/models/assessment-result.js
@@ -70,137 +70,144 @@ module.exports = function (Assessmentresult) {
 							console.log(errorPeer);
 						} else {
 							instancePeer.certificates.create({}, (err, certificateInstanceObj) => {
-								const certificateInstance = certificateInstanceObj.toJSON();
-								const displayHtml = compiled({
-									participantName: assessmentResultInstanceJSON.assessees[0].profiles[0].first_name + ' ' + assessmentResultInstanceJSON.assessees[0].profiles[0].last_name,
-									topics: topicString,
-									assessmentResult: assessmentResultInstanceJSON.assessment_rules[0].value,
-									issuerName: assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].owners[0].profiles[0].first_name + ' ' + assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].owners[0].profiles[0].last_name,
-									difficultyLevel: assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].difficultyLevel,
-									gyanEarned: gyanEarned,
-									expiryDate: certificateTemplate.expiryDate,
-									QRCode: qrcode('https://theblockchainu.com/certificate/' + certificateInstance.id, {
-										size: 125
-									})
-								});
-								const certificate = {
-									"type": "certificate",
-									"id": certificateInstance.id,
-									"issuer": {
-										"id": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].owners[0].id,
-										"ethAddress": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].owners[0].ethAddress,
-										"profile": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].owners[0].profiles[0]
-									},
-									"recipient": {
-										"id": assessmentResultInstanceJSON.assessees[0].id,
-										"ethAddress": assessmentResultInstanceJSON.assessees[0].ethAddress,
-										"profile": assessmentResultInstanceJSON.assessees[0].profiles[0]
-									},
-									"collection": {
-										"id": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].id,
-										"blockchainId": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].id.replace(/\-/g, ''),
-										"type": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].type,
-										"title": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].title,
-										"language": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].language,
-										"headline": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].headline,
-										"description": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].description,
-										"difficultyLevel": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].difficultyLevel,
-										"prerequisites": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].prerequisites,
-										"maxSpots": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].maxSpots,
-										"videoUrls": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].videoUrls,
-										"imageUrls": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].imageUrls,
-										"totalHours": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].totalHours,
-										"price": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].price,
-										"currency": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].currency,
-										"cancellationPolicy": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].cancellationPolicy,
-										"ageLimit": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].ageLimit,
-										"aboutHost": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].aboutHost,
-										"notes": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].notes,
-										"status": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].status,
-										"academicGyan": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].academicGyan,
-										"nonAcademicGyan": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].nonAcademicGyan,
-										"createdAt": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].createdAt,
-										"assessment": {
-											"type": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].type,
-											"style": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].style,
-											"result": assessmentResultInstanceJSON.assessment_rules[0].value,
-											"academicGyanEarned": aGyan,
-											"nonAcademicGyanEarned": naGyan,
+								if (err) {
+									next(err);
+								} else {
+									console.log('CENTRALIZED SERVER DONE. NOW COMPILING DISPLAY HTML FOR VARIABLES');
+									const certificateInstance = certificateInstanceObj.toJSON();
+									const displayHtml = compiled({
+										participantName: assessmentResultInstanceJSON.assessees[0].profiles[0].first_name + ' ' + assessmentResultInstanceJSON.assessees[0].profiles[0].last_name,
+										topics: topicString,
+										assessmentResult: assessmentResultInstanceJSON.assessment_rules[0].value,
+										issuerName: assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].owners[0].profiles[0].first_name + ' ' + assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].owners[0].profiles[0].last_name,
+										difficultyLevel: assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].difficultyLevel,
+										gyanEarned: gyanEarned,
+										expiryDate: certificateTemplate.expiryDate,
+										QRCode: qrcode('https://theblockchainu.com/certificate/' + certificateInstance.id, {
+											size: 125
+										})
+									});
+									console.log('DISPLAY HTML READY: ' + displayHtml);
+									console.log('NOW PREPARING CERTIFICATE JSON');
+									const certificate = {
+										"type": "certificate",
+										"id": certificateInstance.id,
+										"issuer": {
+											"id": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].owners[0].id,
+											"ethAddress": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].owners[0].ethAddress,
+											"profile": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].owners[0].profiles[0]
 										},
-										"participation": {
-											"calendarId": assessmentResultInstanceJSON.calendarId,
-											"cohortStartDateTime": calendar.startDate,
-											"cohortEndDateTime": calendar.endDate
-										}
-									},
-									"verification": {
-										"type": "sha256"
-									},
-									"issuedOn": new Date(),
-									"displayHtml": displayHtml,
-									"universalIdentifier": certificateInstance.id.replace(/\-/g, '')
-								};
-								const hash = sha256(JSON.stringify(certificate));
-								request
-									.put({
-										url: protocolUrl + 'collections/' + assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].id + '/peers/' + assessmentResultInstanceJSON.assessees[0].ethAddress,
-										body: {
-											assessmentResult: assessmentResultInstanceJSON.assessment_rules[0].value,
-											engagementResult: assessmentResultInstanceJSON.assessmentEngagementResult,
-											commitmentResult: assessmentResultInstanceJSON.assessmentCommitmentResult,
-											hash: hash // hash
+										"recipient": {
+											"id": assessmentResultInstanceJSON.assessees[0].id,
+											"ethAddress": assessmentResultInstanceJSON.assessees[0].ethAddress,
+											"profile": assessmentResultInstanceJSON.assessees[0].profiles[0]
 										},
-										json: true
-									}, (err, response, data) => {
-										if (err) {
-											console.error(err);
-										} else if (data.tx) {
-											console.log('Recorded assessment on blockchain: ');
-											console.log(data);
-											const signature = {
-												"type": [
-													"sha256"
-												],
-												"targetHash": hash,
-												"anchors": [
-													{
-														"sourceId": data.tx,
-														"type": "EthData",
-														"chain": "ethereumTestnet"
-													}
-												]
-											};
-											certificate.signature = signature;
-											certificateInstanceObj.stringifiedJSON = JSON.stringify(certificate);
-											certificateInstanceObj.save((error, updatedInstance) => {
-												if (error) {
-													console.log(error);
-												} else {
-													console.log('UpdatedInstance');
-													console.log(displayHtml);
-													let html_body = ' <html> <head> <title>The Blockchain University</title>   </head>  <body> <div>  ' + displayHtml + ' </div> </body> </html>';
-													var attachment = {
-														data: intoStream(updatedInstance.stringifiedJSON),
-														filename: 'certificate.json',
-														knownLength: updatedInstance.stringifiedJSON.length,
-														contentType: 'application/json'
+										"collection": {
+											"id": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].id,
+											"blockchainId": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].id.replace(/\-/g, ''),
+											"type": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].type,
+											"title": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].title,
+											"language": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].language,
+											"headline": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].headline,
+											"description": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].description,
+											"difficultyLevel": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].difficultyLevel,
+											"prerequisites": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].prerequisites,
+											"maxSpots": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].maxSpots,
+											"videoUrls": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].videoUrls,
+											"imageUrls": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].imageUrls,
+											"totalHours": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].totalHours,
+											"price": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].price,
+											"currency": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].currency,
+											"cancellationPolicy": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].cancellationPolicy,
+											"ageLimit": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].ageLimit,
+											"aboutHost": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].aboutHost,
+											"notes": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].notes,
+											"status": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].status,
+											"academicGyan": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].academicGyan,
+											"nonAcademicGyan": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].nonAcademicGyan,
+											"createdAt": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].createdAt,
+											"assessment": {
+												"type": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].type,
+												"style": assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].style,
+												"result": assessmentResultInstanceJSON.assessment_rules[0].value,
+												"academicGyanEarned": aGyan,
+												"nonAcademicGyanEarned": naGyan,
+											},
+											"participation": {
+												"calendarId": assessmentResultInstanceJSON.calendarId,
+												"cohortStartDateTime": calendar.startDate,
+												"cohortEndDateTime": calendar.endDate
+											}
+										},
+										"verification": {
+											"type": "sha256"
+										},
+										"issuedOn": new Date(),
+										"displayHtml": displayHtml,
+										"universalIdentifier": certificateInstance.id.replace(/\-/g, '')
+									};
+									console.log('HASHING CERTIFICATE JSON');
+									const hash = sha256(JSON.stringify(certificate));
+									console.log('CERTIFICATE HASH READY: ' + hash);
+									request
+											.put({
+												url: protocolUrl + 'collections/' + assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].id + '/peers/' + assessmentResultInstanceJSON.assessees[0].ethAddress,
+												body: {
+													assessmentResult: assessmentResultInstanceJSON.assessment_rules[0].value,
+													engagementResult: assessmentResultInstanceJSON.assessmentEngagementResult,
+													commitmentResult: assessmentResultInstanceJSON.assessmentCommitmentResult,
+													hash: hash // hash
+												},
+												json: true
+											}, (err, response, data) => {
+												if (err) {
+													console.error(err);
+												} else if (data && data.tx) {
+													console.log('RECORDED ASSESSMENT ON BLOCKCHAIN: ');
+													console.log(data);
+													const signature = {
+														"type": [
+															"sha256"
+														],
+														"targetHash": hash,
+														"anchors": [
+															{
+																"sourceId": data.tx,
+																"type": "EthData",
+																"chain": "ethereumTestnet"
+															}
+														]
 													};
-													loopback.Email.send({
-														to: assessmentResultInstanceJSON.assessees[0].email,
-														from: 'The Blockchain University <noreply@mx.theblockchainu.com>',
-														subject: 'Your certificate for ' + assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].type + ': ' + assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].title,
-														html: html_body,
-														attachments: [attachment]
+													certificate.signature = signature;
+													certificateInstanceObj.stringifiedJSON = JSON.stringify(certificate);
+													certificateInstanceObj.save((error, updatedInstance) => {
+														if (error) {
+															console.log(error);
+														} else {
+															console.log('SAVED CERTIFICATE JSON WITH HASH IN CENTRAL DB');
+															let html_body = ' <html> <head> <title>The Blockchain University</title>   </head>  <body> <div>  ' + displayHtml + ' </div> </body> </html>';
+															var attachment = {
+																data: intoStream(updatedInstance.stringifiedJSON),
+																filename: 'certificate.json',
+																knownLength: updatedInstance.stringifiedJSON.length,
+																contentType: 'application/json'
+															};
+															loopback.Email.send({
+																to: assessmentResultInstanceJSON.assessees[0].email,
+																from: 'The Blockchain University <noreply@mx.theblockchainu.com>',
+																subject: 'Your certificate for ' + assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].type + ': ' + assessmentResultInstanceJSON.assessment_rules[0].assessment_models[0].collections[0].title,
+																html: html_body,
+																attachments: [attachment]
+															});
+														}
 													});
+												} else {
+													console.log('transaction Id not found');
+													console.log(data);
 												}
 											});
-
-										} else {
-											console.log('transaction Id not found');
-											console.log(data);
-										}
-									});
-								next(null, certificate);
+									next(null, certificate);
+								}
 							});
 						}
 					});
